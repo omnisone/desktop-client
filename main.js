bd1aad67a20d959240882958bfc0f4551a1d70b0
Vishnu Vijayan <vvijayan@purdue.edu>
2018-05-06 22:55:47 -0700

    Make control bar draggable

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ let win
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 1500, height: 800 })
+    win = new BrowserWindow({
+        width: 1500,
+        height: 800,
+        titleBarStyle: 'hiddenInset'
+    })
 
     // and load the index.html of the app.
     win.loadURL(url.format({
