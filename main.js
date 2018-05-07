2a5fd35dff5017abb8161408c0dbbf714ced1796
Vishnu Vijayan <vvijayan@purdue.edu>
2018-05-06 21:57:16 -0700

    Creat prev, play, and next buttons

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -8,7 +8,7 @@ let win
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 1500, height: 800 })
 
     // and load the index.html of the app.
     win.loadURL(url.format({
@@ -26,6 +26,12 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         win = null
+    })
+
+    // Refresh button for development
+    globalShortcut.register('f5', function () {
+        console.log('Refreshing...')
+        win.reload()
     })
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
+
+/* Angular Material */
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PlayerControlsComponent } from './components/player-controls/player-controls.component';
@@ -12,7 +16,9 @@ import { PlayerControlsComponent } from './components/player-controls/player-con
   ],
   imports: [
     BrowserModule,
-    NgxElectronModule
+    BrowserAnimationsModule,
+    NgxElectronModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
