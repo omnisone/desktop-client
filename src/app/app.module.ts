4cb42096a628564ab8c92ab8db73a96df1319fce
Vishnu Vijayan <vvijayan@purdue.edu>
2018-05-06 22:37:21 -0700

    Create basic control layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
+import { FormsModule } from '@angular/forms';
 
 /* Angular Material */
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { 
+  MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatInputModule,
+  MatCardModule 
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PlayerControlsComponent } from './components/player-controls/player-controls.component';
+import { SearchBarComponent } from './components/player-controls/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerControlsComponent
+    PlayerControlsComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     NgxElectronModule,
-    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule
+    FormsModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/player-controls/player-controls.component.ts
+++ src/app/components/player-controls/player-controls.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-player-controls',
   templateUrl: './player-controls.component.html',
-  styleUrls: ['./player-controls.component.css']
+  styleUrls: ['./player-controls.component.scss']
 })
 export class PlayerControlsComponent implements OnInit {
 
