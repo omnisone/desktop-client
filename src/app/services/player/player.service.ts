ce67b34adbd876097294f6678b099951375cf089
Vishnu Vijayan <vvijayan@purdue.edu>
2018-05-09 17:49:57 -0700

    Refactor player service

--- src/app/services/player/player.service.ts
+++ src/app/services/player/player.service.ts
@@ -41,21 +41,6 @@ export class PlayerService {
     })
   }
 
-  private renderTrack(track: Song, url: string, callback?: Function) {
-    if (this.audio) {
-      this.audio.pause()
-    }
-
-    this.audioUrlMap.set(track.id, url)
-    this.audio = new Audio()
-    this.audio.src = url
-    this.audio.play()
-
-    if (callback) {
-      callback()
-    }
-  }
-
   public play() {
     if (!this.audio) {
       console.warn('audio is null')
@@ -74,46 +59,18 @@ export class PlayerService {
     this.audio.pause()
   }
 
-  public test() {
-    console.log('testing')
-    // const torrBuf = fs.readFileSync('test3.txt.torrent')
-    const torrBuf = 'magnet:?xt=urn:btih:be6ba1051b5ac85e5ce112d27f384846f380ef21&dn=DO+YOU+KNOW+DA+WAE+(TRAP+REMIX).mp3&tr=http%3A%2F%2Flocalhost%3A3303&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
-
-    var opts = () => {
-      console.log('called')
-      return {
-        customParam: 'booty',
-        smh: true
-      }
+  private renderTrack(track: Song, url: string, callback?: Function) {
+    if (this.audio) {
+      this.audio.pause()
     }
 
-    const trackers = ['udp://localhost:3303']
+    this.audioUrlMap.set(track.id, url)
+    this.audio = new Audio()
+    this.audio.src = url
+    this.audio.play()
 
-    this.torrentClient.add(torrBuf, { announceList: [trackers], getAnnounceOpts: opts }, function (torrent) {
-      // Got torrent metadata!
-      console.log('Client is downloading:', torrent.infoHash)
-
-      var file = torrent.files.find(function (file) {
-        return file.name.endsWith('.mp3')
-      })
-
-      file.getBlobURL(function (err, url) {
-        if (err) throw err
-        let audio = new Audio()
-        audio.src = url
-        audio.play()
-      })
-
-      // Display the file by adding it to the DOM.
-      // Supports video, audio, image files, and more!
-      // let audio = document.createElement('audio')
-      // document.body.appendChild(audio)
-
-      // Stream the video into the video tag
-      // file.createReadStream().pipe(audio)
-
-      // file.appendTo('body')
-
-    })
+    if (callback) {
+      callback()
+    }
   }
 }
